764a3a215b99d63032dda4ddc303d03b1132bbe6
SonarShroom <mario.shadic.esteves@hotmail.com>
2023-11-25 21:52:05 +0000

    add pause menu

--- ScriptsNew/UI/HUDBehavior.gd
+++ ScriptsNew/UI/HUDBehavior.gd
@@ -1,11 +1,29 @@
 extends CanvasLayer
 
 @export var timer_label : Label;
+@export var continue_btn : Button;
+@export var menu_btn : Button;
+@export var exit_btn : Button;
 @onready var bullseye_scene = load("res://Prefabs/UI/hud_bullseye.tscn");
 
 func _ready():
 	var singletons = Singletons;
 	singletons.Hud = self;
+	var continue_fn = func():
+		get_tree().paused = false;
+		$Control.visible = false;
+	continue_btn.button_down.connect(continue_fn);
+	var menu_fn = func():
+		pass
+	menu_btn.button_down.connect(menu_fn);
+	var exit_fn = func():
+		get_tree().quit();
+	exit_btn.button_down.connect(exit_fn);
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().paused = true;
+		$Control.visible = true;
 
 func spawn_bullseye():
 	var bullseye_inst = bullseye_scene.instantiate();
